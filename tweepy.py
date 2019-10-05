fd6e38de033d2522621540048805fa98ec5ff9b7
sfxcwys <sfxcwys@gmail.com>
2019-10-05 12:01:06 -0400

    Added method to reply to most recent tweet

--- tweepy.py
+++ tweepy.py
@@ -21,8 +21,38 @@ def reply_hashtag_bot(hashtag_list, text=None, filenames=None):
             debug = api.update_status(status=reply, media_ids=media_ids, in_reply_to_status_id=tweetid)
 #             debug = api.update_status(status=reply, in_reply_to_status_id=tweetid)
 #             print(debug)
-     
+
+
+def reply_most_recent_tweet(text, image_list, user_id):
+    """
+    Reply to the most recent tweet made by the given user
     
+    @param text: str, text of the reply tweet
+    @param image_list: list of str, images to be uploaded the the reply tweet
+    @param user_id: str, user ID of the original tweet 
+    """
+    
+    # find the most recent tweet_id from user_id
+    most_recent_tweet_id = find_most_recent_tweet_id(user_id)
+    
+    # upload media
+    media_ids = [api.media_upload(filename).media_id for filename in image_list]
+    
+    # reply to tweet
+    status = api.update_status(status=text, media_ids=media_ids, in_reply_to_status_id = most_recent_tweet_id)
+
+    return
+
+
+def find_most_recent_tweet_id(user_id):
+    """
+    Retrieve the most recent Tweet ID from the given user ID
+    """
+    # assume count=1 will return the most recent tweet
+    query_result = api.user_timeline(user_id=user_id, count=1)
+    return query_result[0]._json['id']
+
+
 if __name__ == "__main__":
     # load env keys
     load_dotenv(find_dotenv())
